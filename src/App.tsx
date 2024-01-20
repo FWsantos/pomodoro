dc6d96e8fd0b9d1ebb5d275cbd3592d56b6c9dae
Wesley Santos <fwsantos23@gmail.com>
2024-01-20 08:25:07 -0300

    feat: function version

--- src/App.tsx
+++ src/App.tsx
@@ -1,36 +1,98 @@
-import { useState } from 'react'
-import UpdateElectron from '@/components/update'
-import logoVite from './assets/logo-vite.svg'
-import logoElectron from './assets/logo-electron.svg'
+import { useEffect, useState } from 'react'
 import './App.css'
 
+interface TimerType {
+  type: 'pomodoro' | 'short-break' | 'long-break';
+}
+
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(1500)
+  const [countType, setCountType] = useState<TimerType>({ type: 'pomodoro' })
+  const [isActive, setIsActive] = useState(false)
+
+  useEffect(() => {
+    let interval: NodeJS.Timeout | null = null
+
+    if (isActive) {
+      interval = setInterval(() => {
+        if (count > 0) {
+          setCount((prevCount) => prevCount - 1)
+        }
+      }, 1000)
+    } else if (!isActive && count !== 0) {
+      clearInterval(interval!)
+    }
+
+    return () => {
+      clearInterval(interval!)
+    }
+  }, [isActive, count])
+
+  useEffect(() => {
+
+    console.log(countType.type)
+    switch (countType.type) {
+      case 'pomodoro':
+        setCount(1500)
+        break
+      case 'short-break':
+        setCount(300)
+        break
+      case 'long-break':
+        setCount(900)
+        break
+      default:
+        setCount(1500)
+    }
+  }, [countType])
+
+  const reset = () => {
+    setCount(1500)
+    setIsActive(false)
+  }
+
+
+  const formatTime = (time: number) => {
+    const minutes = Math.floor(time / 60)
+    const seconds = time % 60
+
+    return `${minutes}:${seconds < 10 ? '0' : ''}${seconds}`
+  }
+
   return (
     <div className='App'>
-      <div className='logo-box'>
-        <a href='https://github.com/electron-vite/electron-vite-react' target='_blank'>
-          <img src={logoVite} className='logo vite' alt='Electron + Vite logo' />
-          <img src={logoElectron} className='logo electron' alt='Electron + Vite logo' />
-        </a>
-      </div>
-      <h1>Electron + Vite + React</h1>
-      <div className='card'>
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
+      <div className='options'>
+        <button
+          className={countType.type === 'pomodoro' ? 'selected' : ''}
+          onClick={() => setCountType({ type: 'pomodoro' })}
+        >
+          Pomodoro
         </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
+        <button
+          className={countType.type === 'short-break' ? 'selected' : ''}
+          onClick={() => setCountType({ type: 'short-break' })}
+        >
+          Short Break
+        </button>
+        <button
+          className={countType.type === 'long-break' ? 'selected' : ''}
+          onClick={() => setCountType({ type: 'long-break' })}
+        >
+          Long Break
+        </button>
       </div>
-      <p className='read-the-docs'>
-        Click on the Electron + Vite logo to learn more
-      </p>
-      <div className='flex-center'>
-        Place static files into the<code>/public</code> folder <img style={{ width: '5em' }} src='./node.svg' alt='Node logo' />
+      <div className='timer'>
+        <p>{formatTime(count)}</p>
       </div>
+      <div className='buttons'>
+        <button onClick={() => setIsActive(!isActive)}>
+          {isActive ? 'Pause' : 'Start'}
+        </button>
 
-      <UpdateElectron />
+        <button onClick={() => reset()}>
+          Reset
+        </button>
+      </div>
     </div>
   )
 }
